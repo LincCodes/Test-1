e2aaa46ed2f4e8b338711b33f01da99450f626e2
LincCodes <linccodes@gmail.com>
2022-11-06 00:23:35 +0000

    Nice

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-app.LoadPlugin( "Picasso" );
+cfg.Light
 
 function OnStart()
 {
-  lay = app.CreateLayout( "Linear", "VCenter,FillXY" );
-
-  img = app.AddImage( lay, "http://droidscript.org/wp-content/uploads/2020/08/cropped-unnamed-5.png", 0.3 );
-  lay.AddChild( img );
-
-  img2 = Picasso.CreateImage( "http://droidscript.org/wp-content/uploads/2020/08/cropped-unnamed-5.png", 0.3, -1, "Circle" );
-  img2.SetMargins( 0, 0.1, 0, 0);
-  lay.AddChild( img2 );
-
-  app.AddLayout( lay );
+	lay  = app.CreateLayout( "Linear", "Horizontal" )
+	app.AddText( lay, "Hello" )
+	app.AddLayout( lay )
 }
-
